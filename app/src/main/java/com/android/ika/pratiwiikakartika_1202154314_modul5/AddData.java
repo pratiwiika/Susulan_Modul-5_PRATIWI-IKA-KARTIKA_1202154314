1f9ec485f72b41b8c21e127a27deb613fac1178e
pratiwiika <pratiwiika19@gmail.com>
2018-03-25 21:56:33 +0700

    MODUL 5

---
+++ app/src/main/java/com/android/ika/pratiwiikakartika_1202154314_modul5/AddData.java
@@ -0,0 +1,41 @@
+package com.android.ika.pratiwiikakartika_1202154314_modul5;
+
+/**
+ * Created by ASUS on 3/25/2018.
+ */
+
+public class AddData {
+    //deklarasi variable
+    String todo, desc, prior;
+
+    //konstruktor
+    public AddData(String todo, String desc, String prior){
+        this.todo=todo;
+        this.desc=desc;
+        this.prior=prior;
+}
+    //method setter dan getter untuk to do , description dan priority
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrior() {
+        return prior;
+    }
+
+    public void setPrior(String prior) {
+        this.prior = prior;
+    }
+}
